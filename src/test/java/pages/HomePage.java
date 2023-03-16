ed0303d7f00ad6e68dc8ea4cc068cdc3fc09e68d
Administrator <Administrator@172.31.20.117>
2023-03-16 08:37:23 +0000

    updates

--- src/test/java/pages/HomePage.java
+++ src/test/java/pages/HomePage.java
@@ -1,5 +1,27 @@
 package pages;
 
-public class HomePage {
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 
+import base.TestBase;
+
+public class HomePage extends TestBase {
+
+@FindBy(xpath="//a[contains(text(),'Samsung galaxy s6')]")
+WebElement item1;
+
+@FindBy(xpath="//a[contains(text(),'Samsung galaxy s7')]")
+WebElement item2;
+
+public HomePage() {
+	PageFactory.initElements(driver, this);
+	
+}
+
+public void selectItem() {
+	item1.click();
+	item2.click();
+}
+	
 }
--- src/test/java/stepDefs/PlaceOrderStepDef.java
+++ src/test/java/stepDefs/PlaceOrderStepDef.java
@@ -1,8 +1,5 @@
 package stepDefs;
 
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.FindBy;
-import org.openqa.selenium.support.PageFactory;
 import base.TestBase;
 
 /**
@@ -11,28 +8,4 @@ import base.TestBase;
  */
 public class PlaceOrderStepDef extends TestBase {
 	
-@FindBy(id="login2")
-WebElement login;
-
-@FindBy(id="loginusername")
-WebElement username;
-
-@FindBy(id="loginpassword")
-WebElement password;
-
-@FindBy(xpath="//*[@onclick=logIn()]")
-WebElement loginButton;
-
-
-public PlaceOrderStepDef(){
-PageFactory.initElements(driver, this );
-}
-
-public void logIn() {
-	login.click();
-	username.sendKeys("suryawan");
-	password.sendKeys("suryawan100%");
-	loginButton.click();
-}
-
 }
