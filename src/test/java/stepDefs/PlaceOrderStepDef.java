d04d50ba1441601e826a2fd2ab50b49baf4a48fd
Administrator <Administrator@172.31.20.117>
2023-03-16 08:19:02 +0000

    update

--- src/test/java/stepDefs/PlaceOrderStepDef.java
+++ src/test/java/stepDefs/PlaceOrderStepDef.java
@@ -1,5 +1,38 @@
 package stepDefs;
 
-public class PlaceOrderStepDef {
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import base.TestBase;
+
+/**
+ * @author Administrator
+ *
+ */
+public class PlaceOrderStepDef extends TestBase {
+	
+@FindBy(id="login2")
+WebElement login;
+
+@FindBy(id="loginusername")
+WebElement username;
+
+@FindBy(id="loginpassword")
+WebElement password;
+
+@FindBy(xpath="//*[@onclick=logIn()]")
+WebElement loginButton;
+
+
+public PlaceOrderStepDef(){
+PageFactory.initElements(driver, this );
+}
+
+public void logIn() {
+	login.click();
+	username.sendKeys("suryawan");
+	password.sendKeys("suryawan100%");
+	loginButton.click();
+}
 
 }
