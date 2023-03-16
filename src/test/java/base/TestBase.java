f22f83ccb64e9df98e11c60658cadd636bcc816b
Administrator <Administrator@172.31.20.117>
2023-03-16 05:56:25 +0000

    TestBase

--- src/test/java/base/TestBase.java
+++ src/test/java/base/TestBase.java
@@ -1,5 +1,53 @@
 package base;
 
-public class TestBase {
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.time.Duration;
+import java.util.Properties;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class TestBase {
+	
+	public static WebDriver driver;
+	public static Properties prop = null;
+	
+	public TestBase() {
+		prop = new Properties();
+		String path = System.getProperty("user.dir")+"//src//test//resources//configFiles//config.properties";
+		FileInputStream fin;
+		try {
+			fin = new FileInputStream(path);
+			prop.load(fin);
+		}
+		catch(FileNotFoundException e) {
+			e.printStackTrace();
+			
+		}
+		catch(IOException e) {
+			e.printStackTrace();
+		}
+	} 
+
+	public static void initialize() {
+		String strBrowser = prop.getProperty("browser");
+		if(strBrowser.equalsIgnoreCase("chrome")) {
+			WebDriverManager.chromedriver().setup();
+			driver = new ChromeDriver();
+		}
+		else if(strBrowser.equalsIgnoreCase("edge")) {
+			WebDriverManager.edgedriver().setup();
+			driver = new EdgeDriver();
+		}
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+		driver.manage().deleteAllCookies();		
+		driver.get(prop.getProperty("url"));
+	}
+	
 }
